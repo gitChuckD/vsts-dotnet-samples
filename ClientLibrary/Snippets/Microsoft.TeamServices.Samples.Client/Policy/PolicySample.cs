2365ffe2ccffed2726e01b25cb8b3679de04e622
Chuck Hanson <chuck.hanson@nebraska.gov>
2019-01-10 14:59:42 -0600

    added code for policy.  compiles but isn't working yet

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Policy/PolicySample.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Policy.WebApi;
+
+namespace Microsoft.TeamServices.Samples.Client.Git
+{
+    [ClientSample(GitWebApiConstants.AreaName, "policy")]
+    public class PoliciesSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public IEnumerable<PolicyConfiguration> ListPolicyConfigurations()
+        {
+            VssConnection connection = this.Context.Connection;
+            PolicyHttpClient policyClient = connection.GetClient<PolicyHttpClient>();
+
+            Guid projectId = ClientSampleHelpers.FindAnyProject(this.Context).Id;
+
+            List<PolicyConfiguration> configs = policyClient.GetPolicyConfigurationsAsync(projectId).Result;
+
+            foreach (PolicyConfiguration config in configs)
+            {
+                Console.WriteLine("{0} {1} {2}", config.Id, config.GetType(), config.Url);
+            }
+
+            return configs;
+        }
+
+    }
+}
